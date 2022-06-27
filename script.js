79c9d620edaeca17c7e0e52d1d022ae38b71125d
BEST-KOMP\Admin <yuri.vynnyk@gmail.com>
2022-06-28 01:39:43 +0300

    add media < 400

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 //burger menu
 const headerBurger = document.querySelector('.header__burger');
 const menu = document.querySelector('.nav');
+
+const toggleMenu = () => {
+  headerBurger.classList.toggle('activeBurger');
+  menu.classList.toggle('activeBurger');
+};
+
 headerBurger.addEventListener('click', function(){
-	headerBurger.classList.toggle('activeBurger');
-	menu.classList.toggle('activeBurger');
+  toggleMenu();
+});
+
+menu.addEventListener('click', (e) => {
+  if(e.target === menu) {
+    toggleMenu();
+  }
 });
